483790a4eb27522807cbd1d27f649deba90c54f5
MoeBrowne <oliver.browne@uplyme.com>
2015-04-20 22:49:41 +0100

    Set the connection string to throw an error only for actions that require them

--- ssh-manager.sh
+++ ssh-manager.sh
@@ -10,6 +10,9 @@ CONNECTION_STRING=$2
 # Defaults!
 KEY_TYPE="rsa"
 
+# Set the key base root
+KEY_PATH_ROOT="/home/$USERNAME/.ssh"
+
 # Get any params defined
 for i in "$@"
 do
@@ -21,24 +24,26 @@ done
 # Split the connection string into the domain and the username
 IFS='@' read KEY_USER KEY_DOMAIN <<< "$CONNECTION_STRING"
 
-# Check both the username and domain have been found correctly
-if [ "$KEY_USER" = "" ]; then
-	echo "ERROR: Please specify a username"
-	exit
+# Only throw an error if the connection string is missing for actions that require them
+if [ "$ACTION" != "list" ]; then
+
+	# Check both the username and domain have been found correctly
+	if [ "$KEY_USER" = "" ]; then
+		echo "ERROR: Please specify a username"
+		exit
+	fi
+	if [ "$KEY_DOMAIN" = "" ]; then
+		echo "ERROR: Please specify a domain"
+		exit
+	fi
+
+	# Set the path the key should be written to
+	KEY_PATH_DIR="$KEY_PATH_ROOT/$KEY_TYPE/$KEY_DOMAIN" #Must be an absolute path!
+
+	# Set the path of the key its self
+	KEY_PATH_KEY="$KEY_PATH_DIR/$KEY_USER"
+
 fi
-if [ "$KEY_DOMAIN" = "" ]; then
-	echo "ERROR: Please specify a domain"
-	exit
-fi
-
-# Set the key base root
-KEY_PATH_ROOT="/home/$USERNAME/.ssh"
-
-# Set the path the key should be written to
-KEY_PATH_DIR="$KEY_PATH_ROOT/$KEY_TYPE/$KEY_DOMAIN" #Must be an absolute path!
-
-# Set the path of the key its self
-KEY_PATH_KEY="$KEY_PATH_DIR/$KEY_USER"
 
 # Get the source directory
 DIR="${BASH_SOURCE%/*}"
