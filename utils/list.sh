d0560d4149b8c64b4921674a567dbcd734bfaf3d
MoeBrowne <oliver.browne@uplyme.com>
2015-04-20 23:04:27 +0100

    Removed the need for the path replacing and got find to just print the names

--- utils/list.sh
+++ utils/list.sh
@@ -1,12 +1,9 @@
 
 # Get all the key types
-KEY_TYPES=`find $KEY_PATH_ROOT/ -mindepth 1 -maxdepth 1 -type d`
+KEY_TYPES=`find $KEY_PATH_ROOT/ -mindepth 1 -maxdepth 1 -type d -printf "%f\n"`
 
 # Loop through all the key types
 for keytype in $KEY_TYPES; do
-
-	# Replace the path name in the key type
-	keytype=${keytype/$KEY_PATH_ROOT\//}
 
 	# Set the path to this type of key
 	keytypepath="$KEY_PATH_ROOT/$keytype"
@@ -15,25 +12,19 @@ for keytype in $KEY_TYPES; do
 	keytype="${keytype^^}"
 
 	# Find all the domains with a key of this type
-	KEY_DOMAINS=`find $keytypepath/ -mindepth 1 -maxdepth 1 -type d`
+	KEY_DOMAINS=`find $keytypepath/ -mindepth 1 -maxdepth 1 -type d -printf "%f\n"`
 
 	# Loop through each domain
 	for keydomain in $KEY_DOMAINS; do
-
-		# Remove the path from the domain name
-		keydomain=${keydomain/$keytypepath\//}
 
 		# Set the path to the key files for this domain
 		keydomainpath="$keytypepath/$keydomain"
 
 		# Get all the keys for this domain
-		KEY_USERS=`find $keydomainpath/ -mindepth 1 -maxdepth 1 -type f ! -name \*.pub`
+		KEY_USERS=`find $keydomainpath/ -mindepth 1 -maxdepth 1 -type f ! -name \*.pub -printf "%f\n"`
 
 		# Loop through each key file we could find
 		for keyuser in $KEY_USERS; do
-
-			# Replace the path from the key name
-			keyuser=${keyuser/$keydomainpath\//}
 			
 			# Get Key length
 			keylength=$(openssl rsa -in "$keydomainpath/$keyuser" -text -noout | grep -oE "[0-9]+ bit")
