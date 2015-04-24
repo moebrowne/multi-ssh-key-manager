781285c85dd53b8b24619632c83c0a99f35b66ba
MoeBrowne <oliver.browne@uplyme.com>
2015-04-24 23:14:43 +0100

    Changed local variables to camelCase

--- utils/list.sh
+++ utils/list.sh
@@ -9,9 +9,9 @@ fi
 declare -A userArray
 
 #Define regex rules
-regexKeyComment="^ssh-rsa .+ (.+)$"
-regexKeyLength="([0-9]+) bit"
-regexKeyFile='^([^\.]+)(.pub)?$'
+regexkeyComment="^ssh-rsa .+ (.+)$"
+regexkeyLength="([0-9]+) bit"
+regexkeyFile='^([^\.]+)(.pub)?$'
 
 # Get all the key types
 KEY_TYPES=`find $KEY_PATH_ROOT/ -mindepth 1 -maxdepth 1 -type d -printf "%f\n"`
@@ -20,28 +20,28 @@ KEY_TYPES=`find $KEY_PATH_ROOT/ -mindepth 1 -maxdepth 1 -type d -printf "%f\n"`
 echo -e "Type\033[10GLength\033[24GUser\033[40GDomain\033[66GFlags\033[79GComment"
 
 # Loop through all the key types
-for keytype in $KEY_TYPES; do
+for keyType in $KEY_TYPES; do
 
 	# Set the path to this type of key
-	keytypepath="$KEY_PATH_ROOT/$keytype"
+	keyTypePath="$KEY_PATH_ROOT/$keyType"
 
 	# Find all the domains with a key of this type
-	KEY_DOMAINS=`find $keytypepath/ -mindepth 1 -maxdepth 1 -type d -printf "%f\n"`
+	KEY_DOMAINS=`find $keyTypePath/ -mindepth 1 -maxdepth 1 -type d -printf "%f\n"`
 
 	# Loop through each domain
-	for keydomain in $KEY_DOMAINS; do
+	for keyDomain in $KEY_DOMAINS; do
 
 		# Set the path to the key files for this domain
-		keydomainpath="$keytypepath/$keydomain"
+		keyDomainPath="$keyTypePath/$keyDomain"
 
 		# Get all the keys for this domain
-		KEY_USERS=`find $keydomainpath/ -mindepth 1 -maxdepth 1 -type f -printf "%f\n"`
+		KEY_USERS=`find $keyDomainPath/ -mindepth 1 -maxdepth 1 -type f -printf "%f\n"`
 
 		# Loop through each key file we could finds
-		for keyfile in $KEY_USERS; do
+		for keyFile in $KEY_USERS; do
 
 			# Get the user name from this key
-			[[ $keyfile =~ $regexKeyFile ]]
+			[[ $keyFile =~ $regexkeyFile ]]
 			keyUsername="${BASH_REMATCH[1]}"
 
 			# Check if we have already displayed the key for this user
@@ -53,52 +53,52 @@ for keytype in $KEY_TYPES; do
 			userArray[$keyUsername]=true
 
 			# Set the paths for the pub and private keys
-			keyPathPub="$keydomainpath/$keyUsername.pub"
-			keyPathPriv="$keydomainpath/$keyUsername"
+			keyPathPub="$keyDomainPath/$keyUsername.pub"
+			keyPathPriv="$keyDomainPath/$keyUsername"
 
 			# Resets
-			keylength="????"
-			keyflags=""
-			keycomment=""
+			keyLength="????"
+			keyFlags=""
+			keyComment=""
 
 			# Check if the public key can be found
 			if [ -f "$keyPathPub" ]; then
 
 				# Get the comment from the key
-				[[ `cat "$keyPathPub"` =~ $regexKeyComment ]]
-				keycomment="${BASH_REMATCH[1]}"
+				[[ `cat "$keyPathPub"` =~ $regexkeyComment ]]
+				keyComment="${BASH_REMATCH[1]}"
 
-				keyflags="$keyflags[PUB] "
+				keyFlags="$keyFlags[PUB] "
 			else
-				keyflags="$keyflags[$COLOUR_RED""NO PUB$COLOUR_RST] "
+				keyFlags="$keyFlags[$COLOUR_RED""NO PUB$COLOUR_RST] "
 			fi
 
 			# Check if the private key can be found
 			if [ -f "$keyPathPriv" ]; then
 
 				# Get Key length
-				[[ `$EXEC_OPENSSL $keytype -in "$keyPathPriv" -text -noout` =~ $regexKeyLength ]]
-				keylength="${BASH_REMATCH[1]}"
+				[[ `$EXEC_OPENSSL $keyType -in "$keyPathPriv" -text -noout` =~ $regexkeyLength ]]
+				keyLength="${BASH_REMATCH[1]}"
 
 				# Check if the key is of a proper length
-				if [[ $keylength -lt "$KEY_MIN_LENGH" ]] || [[ $keylength -eq "$KEY_MIN_LENGH" ]]; then
-					keylength="$COLOUR_RED_BAK$keylength bit$COLOUR_RST"
+				if [[ $keyLength -lt "$KEY_MIN_LENGH" ]] || [[ $keyLength -eq "$KEY_MIN_LENGH" ]]; then
+					keyLength="$COLOUR_RED_BAK$keyLength bit$COLOUR_RST"
 				else
-					keylength="$COLOUR_YEL$keylength bit$COLOUR_RST"
+					keyLength="$COLOUR_YEL$keyLength bit$COLOUR_RST"
 				fi
 
-				keyflags="$keyflags[PRIV] "
+				keyFlags="$keyFlags[PRIV] "
 			else
-				keyflags="$keyflags[$COLOUR_RED""NO PRIV$COLOUR_RST] "
+				keyFlags="$keyFlags[$COLOUR_RED""NO PRIV$COLOUR_RST] "
 			fi
 
 			# Check if we should show the keys path
 			if [ $KEY_PATH_SHOW = true ]; then
-				keypathcomment=" => $keyPathPriv"
+				keyPathComment=" => $keyPathPriv"
 			fi
 
 			# Show the information
-			echo -e "${keytype^^}\033[10G$keylength\033[24G$COLOUR_CYN$keyUsername$COLOUR_RST\033[40G$COLOUR_PUR$keydomain$COLOUR_RST\033[66G$keyflags\033[79G$COLOUR_GRY$keycomment$COLOUR_RST	$keypathcomment"
+			echo -e "${keyType^^}\033[10G$keyLength\033[24G$COLOUR_CYN$keyUsername$COLOUR_RST\033[40G$COLOUR_PUR$keyDomain$COLOUR_RST\033[66G$keyFlags\033[79G$COLOUR_GRY$keyComment$COLOUR_RST	$keyPathComment"
 		done
 	done
 done
