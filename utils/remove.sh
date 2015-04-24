1ca4b7c11819d05605201e1fa631a002e2034d4f
MoeBrowne <oliver.browne@uplyme.com>
2015-04-24 19:25:22 +0100

    Changed the remove action to remove both the private and public keys

--- utils/remove.sh
+++ utils/remove.sh
@@ -1,12 +1,24 @@
 
-# Check the key exists
-if [ -f "$KEY_PATH_KEY" ]; then
-	# Remove the key
-	shred -zu "$KEY_PATH_KEY"
+# Check the private key exists
+if [ -f "$KEY_PATH_PRIV" ]; then
+	# Remove the keys
+	shred -zu "$KEY_PATH_PRIV"
 
-	# Tell the user the key has been removed
-	echo "The key for $KEY_USER@$KEY_DOMAIN has been removed"
+	# Tell the user the private key has been removed
+	echo "$KEY_USER@$KEY_DOMAIN: Removed the private key"
 else
-	# Tell the user the key cant be removed
-	echo "$KEY_USER@$KEY_DOMAIN doesn't have a key..."
+	# Tell the user the private key cant be removed
+	echo "$KEY_USER@$KEY_DOMAIN: No private key found"
+fi
+
+# Check the public key exists
+if [ -f "$KEY_PATH_PUB" ]; then
+	# Remove the key
+	shred -zu "$KEY_PATH_PUB"
+
+	# Tell the user the private key has been removed
+	echo "$KEY_USER@$KEY_DOMAIN: Removed the public key"
+else
+	# Tell the user the private key cant be removed
+	echo "$KEY_USER@$KEY_DOMAIN: No public key found"
 fi
